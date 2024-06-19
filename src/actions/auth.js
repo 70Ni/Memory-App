1cbeb1addb9e0912ef32c9b719b631d0a10cbaec
70ni <4thedevelopments@gmail.com>
2024-06-19 19:14:20 +0530

    user login action

---
+++ src/actions/auth.js
@@ -0,0 +1,17 @@
+import * as api from "../api/index";
+
+export const signin = (formData, history) => async (dispatch) => {
+  try {
+    Navigator("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = (formData, history) => async (dispatch) => {
+  try {
+    Navigator("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
