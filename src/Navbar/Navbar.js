2b50e895500ba43bf5214b0777e0d641e211b7c7
70ni <4thedevelopments@gmail.com>
2024-06-20 13:19:53 +0530

    Nav bar changes

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,32 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Auth from "../Components/Auth/Auth";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+// import { LOGOUT } from "../constants/actionConstants";
+import * as actionType from "../constants/actionConstants";
 
 function Navbar() {
-  const user = null;
+  const [user, setuser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const location = useLocation();
+  const history = useDispatch();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const token = user?.token;
+    setuser(JSON.parse(localStorage.getItem("profile")));
+    console.log(user);
+  }, [location]);
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT", data: null });
+    history("/");
+    setuser(null);
+  };
+
+  console.log(user);
   return (
     <div>
-      <div className="Nav-container">
+      <div
+        className="Nav-container"
+        style={{ display: "flex", alignItems: "center" }}
+      >
         <h1 component={Link} to="/" className="typography-Header">
           Memories
         </h1>
         <div className="Toolbar-section">
           {user ? (
-            <div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "spaceBetween",
+                alignItems: "center",
+              }}
+            >
               <img
                 className="profile"
                 alt={user.result.name}
-                src={user.result.imgageUrl}
-              >
-                {user.result.name.charAt(0)}
-              </img>
+                src={user.result.imageUrl}
+                referrerpolicy="no-referrer"
+                style={{
+                  padding: 20,
+                  borderRadius: 80,
+                  width: 40,
+                  height: "auto",
+                }}
+              />
+
               <div className="user-name">{user.result.name}</div>
-              <button>Logout</button>
+              <button onClick={logout}>Logout</button>
             </div>
           ) : (
-            <button component={Link} to="/auth">
-              Sign In
-            </button>
+            <div style={{ zIndex: 5, padding: 20 }}>
+              <Link to="/auth">
+                <button type="button">sign In</button>
+              </Link>
+              {/* <Link to="/home">logout</Link> */}
+            </div>
           )}
         </div>
       </div>
