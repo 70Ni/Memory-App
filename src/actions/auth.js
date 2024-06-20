dd5a106f977287cb267b1fda339bb00a98613567
70ni <4thedevelopments@gmail.com>
2024-06-20 13:16:42 +0530

    Auth actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,16 +1,26 @@
 import * as api from "../api/index";
+import { AUTH } from "../constants/actionConstants";
+
+// import { useNavigate } from "react-router-dom";
+// const navigate =  ();
 
 export const signin = (formData, history) => async (dispatch) => {
   try {
-    Navigator("/");
+    const { data } = await api.signIn(formData);
+    dispatch({ type: AUTH, data });
+    history("/");
   } catch (error) {
     console.log(error);
   }
 };
 
 export const signup = (formData, history) => async (dispatch) => {
+  console.log(formData);
   try {
-    Navigator("/");
+    const { data } = await api.signUp(formData);
+    console.log(data);
+    dispatch({ type: AUTH, payload: data });
+    history("/");
   } catch (error) {
     console.log(error);
   }
